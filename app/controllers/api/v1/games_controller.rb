7b6b2dce42ecb0bbe92bffe88c705fadf0d4744a
Angela Duran <duranangela19@gmail.com>
2018-08-29 15:26:30 -0600

    Add post route for game, add some logic to game controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -9,6 +9,21 @@ module Api
           render status: 400
         end
       end
+
+      def create
+        game_attributes = {
+                        player_1: User.find_by_api_key(request.headers['X-API-Key']),
+                        player_2: User.find_by_email(params[:opponenet_email]),
+                        player_1_board: Board.new(4),
+                        player_2_board: Board.new(4),
+                        player_1_turns: 0,
+                        player_2_turns: 0,
+                        current_turn: "challenger"
+                      }
+
+        binding.pry
+        game  = Game.create()
+      end
     end
   end
 end
