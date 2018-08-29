5aaf90f3b6cc0a36f0f22a4880c9d80a11d57a50
JP Lynch <JPLynch35@gmail.com>
2018-08-29 17:14:20 -0600

    add json render to game create

--- spec/requests/user_can_create_a_game_spec.rb
+++ spec/requests/user_can_create_a_game_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'POST /api/v1/games' do
   it 'user can create a game' do
-    user1 = User.create!(name: 'Angela', email: 'Bob@Bob.Bob', password: 'Bob', status: 1, api_key: SecureRandom.hex(32))
-    user2 = User.create!(name: 'JP', email: 'JP@Bob.Bob', password: 'JP', status: 1, api_key: SecureRandom.hex(32))
+    user1 = User.create(name: 'Angela', email: 'Bob@Bob.Bob', password: 'Bob', status: 1, api_key: SecureRandom.hex(32))
+    user2 = User.create(name: 'JP', email: 'JP@Bob.Bob', password: 'JP', status: 1, api_key: SecureRandom.hex(32))
 
     # Faraday.post '/api/v1/games' do |faraday|
     #   faraday.headers["X-API-Key"] = user1.api_key
