70958790b5db1ccfabccc487c3e953e58d1bba0e
JP Lynch <JPLynch35@gmail.com>
2018-08-29 18:49:24 -0600

    add first ship placement

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,28 +1,24 @@
-module Api
-  module V1
-    class GamesController < ActionController::API
-      def show
-        game = Game.find_by_id(params[:id])
-        if game
-          render json: game
-        else
-          render status: 400
-        end
-      end
-
-      def create
-        game_attributes = {
-                        player_1_key: request.headers['X-API-Key'],
-                        player_2_key: User.find_by_email(params[:opponent_email]).api_key,
-                        player_1_board: Board.new(4),
-                        player_2_board: Board.new(4),
-                        player_1_turns: 0,
-                        player_2_turns: 0,
-                        current_turn: "challenger"
-                      }
-        game = Game.create(game_attributes)
-        render json: game
-      end
+class Api::V1::GamesController < ActionController::API
+  def show
+    game = Game.find_by_id(params[:id])
+    if game
+      render json: game
+    else
+      render status: 400
     end
+  end
+
+  def create
+    game_attributes = {
+                    player_1_key: request.headers['X-API-Key'],
+                    player_2_key: User.find_by_email(params[:opponent_email]).api_key,
+                    player_1_board: Board.new(4),
+                    player_2_board: Board.new(4),
+                    player_1_turns: 0,
+                    player_2_turns: 0,
+                    current_turn: "challenger"
+                  }
+    game = Game.create(game_attributes)
+    render json: game
   end
 end
--- spec/requests/user_can_place_first_ship_spec.rb
+++ spec/requests/user_can_place_first_ship_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'POST /api/v1/games/:id/ship' do
+describe 'POST /api/v1/games/:id/ships' do
   it 'user can place first ship' do
     user1 = User.create(name: 'Angela', email: 'Bob@Bob.Bob', password: 'Bob', status: 1, api_key: SecureRandom.hex(32))
     user2 = User.create(name: 'JP', email: 'JP@Bob.Bob', password: 'JP', status: 1, api_key: SecureRandom.hex(32))
@@ -21,11 +21,10 @@ describe 'POST /api/v1/games/:id/ship' do
     }.to_json
     headers = {"X-API-Key" => user1.api_key, "CONTENT_TYPE" => "application/json" }
 
-    post "/api/v1/games/#{game.id}/ship", params: json_payload, headers: headers
-    
-    expect(response).to be(success)
+    post "/api/v1/games/#{game.id}/ships", params: json_payload, headers: headers
+
+    expect(response.status).to be(200)
     expect(response.body[:message]).to eq("Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2.")
-    require 'pry'; binding.pry
     expect(game_attributes[:player_1_board].board.first.first['A1'].contents).to be(Ship)
   end
 end
