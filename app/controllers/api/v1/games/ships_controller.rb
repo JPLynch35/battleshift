44a63fbf252cc78199362011b80f1a426a93ac01
JP Lynch <JPLynch35@gmail.com>
2018-08-30 12:26:34 -0600

    add strong params to ships controller for ship placement

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
     game = Game.find(params[:game_id])
-    body = JSON.parse(request.raw_post, symbolize_names: true)
     if request.headers['X-API-KEY'] == game.player_1_key
       board = game.player_1_board
     else
@@ -9,19 +8,24 @@ class Api::V1::Games::ShipsController < ApiController
     end
     ShipPlacer.new(
       board: board,
-      ship: Ship.new(body[:ship_size]),
-      start_space: body[:start_space],
-      end_space: body[:end_space]
+      ship: Ship.new(ship_params[:ship_size]),
+      start_space: ship_params[:start_space],
+      end_space: ship_params[:end_space]
     ).run
     if request.headers['X-API-KEY'] == game.player_1_key
       game.update_attribute(:player_1_board, board)
     else
       game.update_attribute(:player_2_board, board)
     end
-    if body[:ship_size] == 3
-      render json: game, message: "Successfully placed ship with a size of #{body[:ship_size]}. You have 1 ship(s) to place with a size of 2."
+    if ship_params[:ship_size] == 3
+      render json: game, message: "Successfully placed ship with a size of #{ship_params[:ship_size]}. You have 1 ship(s) to place with a size of 2."
     else
-      render json: game, message: "Successfully placed ship with a size of #{body[:ship_size]}. You have 0 ship(s) to place."
+      render json: game, message: "Successfully placed ship with a size of #{ship_params[:ship_size]}. You have 0 ship(s) to place."
     end
   end
+
+  private
+    def ship_params
+      params.require('ship').permit(:ship_size, :start_space, :end_space)
+    end
 end
