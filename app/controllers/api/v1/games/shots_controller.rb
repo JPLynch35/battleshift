d739cbac9aa8fbdc72eb0265e3ac3d10fb276e67
JP Lynch <JPLynch35@gmail.com>
2018-09-01 10:59:21 -0600

    remove files no longer required in pvp setting

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -3,25 +3,31 @@ class Api::V1::Games::ShotsController < ApiController
     game = Game.find(params[:game_id])
     if User.find_by_api_key(request.headers['X-API-Key'])
       turn_processor = TurnProcessor.new(game, params[:shot][:target])
-      if !game.player_1_board.space_names.include?(params[:shot][:target])
-        render status: 400, json: game, message: "Invalid coordinates."
-      elsif !game.winner.nil?
-        render status: 400, json: game, message: "Invalid move. Game over."
-      else
-        if game.current_turn == 'challenger' && request.headers['X-API-KEY'] == game.player_1_key
-          turn_processor.run_player_1!
-          render json: game, message: turn_processor.message
-        elsif game.current_turn == 'opponent' && request.headers['X-API-KEY'] == game.player_2_key
-          turn_processor.run_player_2!
-          render json: game, message: turn_processor.message
-        elsif request.headers['X-API-KEY'] == game.player_1_key || request.headers['X-API-KEY'] == game.player_2_key
-          render status: 400, json: game, message: "Invalid move. It's your opponent's turn"
+
+
+          if !game.player_1_board.space_names.include?(params[:shot][:target])
+            render status: 400, json: game, message: "Invalid coordinates."
+          elsif !game.winner.nil?
+            render status: 400, json: game, message: "Invalid move. Game over."
+          else
+
+
+                if game.current_turn == 'challenger' && request.headers['X-API-KEY'] == game.player_1_key
+                  turn_processor.run_player_1!
+                  render json: game, message: turn_processor.message
+                elsif game.current_turn == 'opponent' && request.headers['X-API-KEY'] == game.player_2_key
+                  turn_processor.run_player_2!
+                  render json: game, message: turn_processor.message
+                elsif request.headers['X-API-KEY'] == game.player_1_key || request.headers['X-API-KEY'] == game.player_2_key
+                  render status: 400, json: game, message: "Invalid move. It's your opponent's turn"
+                else
+                  render json: {message: "Unauthorized"}, status: 401
+                end
+
+                
+              end
         else
           render json: {message: "Unauthorized"}, status: 401
         end
-      end
-    else
-      render json: {message: "Unauthorized"}, status: 401
-    end
   end
 end
